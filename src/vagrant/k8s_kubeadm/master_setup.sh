040dab5d7533637066015aff8468ba86a6cd2213
Guo Ruijing <ruijing.guo@intel.com>
2017-05-03 11:49:50 -0700

    Add k8s installation by vagrant + kubeadm
    Change-Id: I4d858e1d0d6779f9f78c0a74ee032ef92d8a4eb8
    Signed-off-by: Guo Ruijing <ruijing.guo@intel.com>

---
+++ src/vagrant/k8s_kubeadm/master_setup.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+set -e
+HOME=`pwd`
+
+cat << EOF | sudo tee /etc/hosts
+127.0.0.1    localhost
+192.168.1.10 master
+192.168.1.21 worker1
+192.168.1.22 worker2
+192.168.1.23 worker3
+EOF
+
+curl -s http://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+cat <<EOF | sudo tee /etc/apt/sources.list.d/kubernetes.list
+deb http://apt.kubernetes.io/ kubernetes-xenial main
+EOF
+sudo apt-get update
+sudo apt-get install -y docker.io
+sudo apt-get install -y kubelet kubeadm kubectl kubernetes-cni
+
+sudo kubeadm init --apiserver-advertise-address 192.168.1.10  --service-cidr=192.168.1.0/24 --pod-network-cidr=10.244.0.0/16 --token 8c5adc.1cec8dbf339093f0
+sudo cp /etc/kubernetes/admin.conf $HOME/
+sudo chown $(id -u):$(id -g) $HOME/admin.conf
+export KUBECONFIG=$HOME/admin.conf
+echo "export KUBECONFIG=$HOME/admin.conf" >> $HOME/.bash_profile
+
+kubectl apply -f http://git.io/weave-kube-1.6
+#kubectl apply -f http://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
+#kubectl apply -f http://docs.projectcalico.org/v2.1/getting-started/kubernetes/installation/hosted/kubeadm/1.6/calico.yaml
