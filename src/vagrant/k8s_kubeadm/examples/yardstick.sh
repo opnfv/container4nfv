3afa368a08ee3e0581d77f85a444273959aa0565
Guo Ruijing <ruijing.guo@intel.com>
2017-06-19 11:31:36 -0700

    Add yardstick example
    Change-Id: I7c0d4a8d7957ee5eb0efd39e75c561102fa9e06a
    Signed-off-by: Guo Ruijing <ruijing.guo@intel.com>

---
+++ src/vagrant/k8s_kubeadm/examples/yardstick.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+rm -rf id_rsa authorized_keys
+ssh-keygen  -t rsa -N ''  -f id_rsa
+mv id_rsa.pub authorized_keys
+
+kubectl delete rc --all
+kubectl delete pods --all
+kubectl delete configmap --all
+kubectl create configmap yardstick-pub --from-file=authorized_keys
+kubectl create configmap yardstick-prv --from-file=id_rsa
+kubectl create -f /vagrant/examples/yardstick.yaml
+sleep 120
+kubectl get nodes
+kubectl get services
+kubectl get pods
+kubectl get rc
+kubectl describe pods | grep -e "IP:" -e "^Name:"
