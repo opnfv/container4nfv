295a25b365fc7b500860f45fff57a71da5720d6a
Guo Ruijing <ruijing.guo@intel.com>
2017-08-03 17:54:29 -0700

    DONT REVIEW: Fix some issues
    Change-Id: Ib919a14a74c6d33c74c7d4f9baa5fd81b9054cbd
    Signed-off-by: Guo Ruijing <ruijing.guo@intel.com>

--- src/cni/ovsdpdk/install_cni.sh
+++ src/cni/ovsdpdk/install_cni.sh
@@ -4,5 +4,5 @@ set -ex
 cp /cni/ovsdpdk /opt/cni/bin
 cp /cni/setup_ovsdpdk.sh /opt/cni/bin
 cp /cni/teardown_ovsdpdk.sh /opt/cni/bin
-cp /etc/kube-ovsdpdk/cni-conf.json  /etc/cni/net.d/10-ovsdpdk.conf
+cp /etc/kube-cniovsdpdk/cni-conf.json  /etc/cni/net.d/10-ovsdpdk.conf
 while true; do sleep 3600; done
