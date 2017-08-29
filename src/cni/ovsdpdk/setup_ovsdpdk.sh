f3cbd8b36f3ae23dcae56df6b98a8a496f5ad8df
Guo Ruijing <ruijing.guo@intel.com>
2017-08-29 13:32:55 -0700

    [DONOT REVIEW]work around local-host ip route issue in ovsdpdk cni
    Change-Id: Ia25fb3165aca40ee9b510a5133212b7867c4ef0b
    Signed-off-by: Guo Ruijing <ruijing.guo@intel.com>

--- src/cni/ovsdpdk/setup_ovsdpdk.sh
+++ src/cni/ovsdpdk/setup_ovsdpdk.sh
@@ -7,8 +7,17 @@ pid=$(echo $netns | cut -f3 -d"/")
 
 sudo ovs-vsctl --may-exist add-br br-dpdk -- set bridge br-dpdk datapath_type=netdev
 sudo ovs-vsctl --may-exist add-port br-dpdk vhost-user-$pid -- set Interface vhost-user-$pid type=dpdkvhostuser
+
 sudo ln -sf $netns /var/run/netns/$pid
 sudo ip link add dummy-$pid type dummy
 sudo ip link set dummy-$pid netns $pid
+
+sudo ip link add veth-b-$pid type veth peer name veth-c-$pid
+sudo ovs-vsctl --may-exist add-port br-dpdk veth-b-$pid
+sudo ip link set veth-c-$pid netns $pid
+sudo ip netns exec $pid ip link set veth-c-$pid name eth0
+sudo ip netns exec $pid ip link set eth0 up
+sudo ip netns exec $pid ip a a $ip/16 dev eth0
+
 sudo mkdir -p /var/run/cni
 echo $ip | sudo tee /var/run/cni/netconf-$pid
