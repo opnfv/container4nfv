2b8607df937482d1881f264f85567d6e770dca47
Guo Ruijing <ruijing.guo@intel.com>
2017-08-29 13:32:55 -0700

    work around local-host ip route issue in ovsdpdk cni
    Change-Id: Ia25fb3165aca40ee9b510a5133212b7867c4ef0b
    Signed-off-by: Guo Ruijing <ruijing.guo@intel.com>

--- src/cni/ovsdpdk/setup_ovsdpdk.sh
+++ src/cni/ovsdpdk/setup_ovsdpdk.sh
@@ -7,8 +7,10 @@ pid=$(echo $netns | cut -f3 -d"/")
 
 sudo ovs-vsctl --may-exist add-br br-dpdk -- set bridge br-dpdk datapath_type=netdev
 sudo ovs-vsctl --may-exist add-port br-dpdk vhost-user-$pid -- set Interface vhost-user-$pid type=dpdkvhostuser
+
 sudo ln -sf $netns /var/run/netns/$pid
 sudo ip link add dummy-$pid type dummy
 sudo ip link set dummy-$pid netns $pid
+
 sudo mkdir -p /var/run/cni
 echo $ip | sudo tee /var/run/cni/netconf-$pid
--- src/vagrant/k8s_kubeadm/ovsdpdk/start.sh
+++ src/vagrant/k8s_kubeadm/ovsdpdk/start.sh
@@ -7,6 +7,7 @@ do
     sudo sysctl -w vm.nr_hugepages=2048; sleep 1
 done
 sudo modprobe uio_pci_generic
+ip=$(ip a s enp0s9 | grep inet | grep -v inet6 | sed "s/.*inet//" | cut -f2 -d' ')
 sudo ip address flush enp0s9
 sudo /usr/share/dpdk/tools/dpdk_nic_bind.py --bind=uio_pci_generic enp0s9
 sudo sysctl -w vm.nr_hugepages=1024
@@ -20,6 +21,8 @@ sudo pkill ovs-vswitchd
 sudo ovs-vswitchd --dpdk -c 0x1 -n 2 -m $memory -- unix:/var/run/openvswitch/db.sock -vconsole:emer -vsyslog:err -vfile:info --mlockall --no-chdir --log-file=/var/log/openvswitch/ovs-vswitchd.log --pidfile=/var/run/openvswitch/ovs-vswitchd.pid --detach --monitor
 sudo ovs-vsctl add-br br-dpdk -- set bridge br-dpdk datapath_type=netdev
 sudo ovs-vsctl add-port br-dpdk dpdk0 -- set Interface dpdk0 type=dpdk
+sudo ip a a $ip dev br-dpdk
+sudo ip link set dev br-dpdk up
 while true; do sleep 3600; done
 echo sudo docker build -t openretriever/ubuntu1604-ovsdpdk .
 echo sudo docker run -ti --privileged --net=host -v /dev:/dev -v /usr/local/bin:/usr/local/bin -v /var/run/openvswitch/:/var/run/openvswitch/ -v /lib/modules/:/lib/modules openretriever/ubuntu1604-ovsdpdk bash
