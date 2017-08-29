da651841c8259109590e79dab09d8818a8a52d9c
Guo Ruijing <ruijing.guo@intel.com>
2017-08-29 13:32:55 -0700

    [DONOT REVIEW]work around local-host ip route issue in ovsdpdk cni
    Change-Id: Ia25fb3165aca40ee9b510a5133212b7867c4ef0b
    Signed-off-by: Guo Ruijing <ruijing.guo@intel.com>

--- src/vagrant/k8s_kubeadm/master_setup.sh
+++ src/vagrant/k8s_kubeadm/master_setup.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-sudo ifconfig br-dpdk 10.244.0.1/16 up
-
 sudo kubeadm init --apiserver-advertise-address 192.168.1.10  --service-cidr=192.168.1.0/24 --pod-network-cidr=10.244.0.0/16 --token 8c5adc.1cec8dbf339093f0
 sudo cp /etc/kubernetes/admin.conf $HOME/
 sudo chown $(id -u):$(id -g) $HOME/admin.conf
