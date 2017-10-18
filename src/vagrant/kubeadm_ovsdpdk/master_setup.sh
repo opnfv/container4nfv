eecc28839b7088f6bd6be9538db7ddc1986b369f
Guo Ruijing <ruijing.guo@intel.com>
2017-10-18 19:59:55 -0400

    seperate k8s basic installation and ovsdpdk installation
    Change-Id: I9ba3b52feebb7be6037d88f07f8c861ddbfe9bdf
    Signed-off-by: Guo Ruijing <ruijing.guo@intel.com>

---
+++ src/vagrant/kubeadm_ovsdpdk/master_setup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+sudo kubeadm init --apiserver-advertise-address 192.168.1.10  --service-cidr=192.168.1.0/24 --pod-network-cidr=10.244.0.0/16 --token 8c5adc.1cec8dbf339093f0
+sudo cp /etc/kubernetes/admin.conf $HOME/
+sudo chown $(id -u):$(id -g) $HOME/admin.conf
+export KUBECONFIG=$HOME/admin.conf
+echo "export KUBECONFIG=$HOME/admin.conf" >> $HOME/.bash_profile
+
+kubectl apply -f /vagrant/ovsdpdk/kube_ovsdpdk.yml
+kubectl apply -f /src/cni/ovsdpdk/kube_cniovsdpdk.yml
