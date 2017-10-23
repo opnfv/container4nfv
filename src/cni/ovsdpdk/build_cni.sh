f1a65fbba53947492d78c4478fa6c400e629923e
Guo Ruijing <ruijing.guo@intel.com>
2017-10-23 00:49:35 +0000

    build ovsdpdk cni by docker
    Change-Id: I1b1ed822dbfca0c1a94129ee8c0464bfec2e054b
    Signed-off-by: Guo Ruijing <ruijing.guo@intel.com>
    Signed-off-by: vagrant <vagrant@control>
    Signed-off-by: Guo Ruijing <ruijing.guo@intel.com>

--- src/cni/ovsdpdk/build_cni.sh
+++ src/cni/ovsdpdk/build_cni.sh
@@ -1,19 +1,13 @@
 #!/bin/bash
-
 set -ex
 
 sudo apt-get update
-sudo apt-get install -y docker.io devscripts git
+sudo apt-get install -y docker.io devscripts git jq
 wget -qO- https://storage.googleapis.com/golang/go1.8.3.linux-amd64.tar.gz | sudo tar -C /usr/local -xz
 echo 'export GOPATH=/go; export PATH=/usr/local/go/bin:$GOPATH/bin:$PATH' >> ~/.bashrc
 export GOPATH=/go; export PATH=/usr/local/go/bin:$GOPATH/bin:$PATH
-git clone https://github.com/containernetworking/cni
-echo sudo CNI_PATH=$CNI_PATH ./priv-net-run.sh ifconfig
 git clone https://github.com/containernetworking/plugins
 cd plugins
 git checkout 5544d9ced0d6e908fe26e9dbe529c7feb87d21f5
-patch -p1 < /vagrant/ovsdpdk.patch
-./build.sh
-cd bin
-cp /vagrant/* .
-sudo docker build -t openretriever/cni-ovsdpdk .
+patch -p1 < ../ovsdpdk.patch
+bash ./build.sh
