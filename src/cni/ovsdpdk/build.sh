f9957f277f5a976e867a8225cbd0fa68e45440c6
Guo Ruijing <ruijing.guo@intel.com>
2017-10-23 00:49:35 +0000

    build ovsdpdk cni by docker
    Change-Id: I1b1ed822dbfca0c1a94129ee8c0464bfec2e054b
    Signed-off-by: Guo Ruijing <ruijing.guo@intel.com>
    Signed-off-by: vagrant <vagrant@control>
    Signed-off-by: Guo Ruijing <ruijing.guo@intel.com>

---
+++ src/cni/ovsdpdk/build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -ex
+
+HOME="$(dirname `readlink -f $0`)"
+PWD="$(pwd)"
+
+cd $HOME
+sudo apt-get install -y docker.io
+sudo docker rmi -f cniovsdpdk.build || true
+sudo docker build -t cniovsdpdk.build . -f Dockerfile.build
+sudo docker run -v $HOME/cni:/build -t cniovsdpdk.build cp /plugins/bin/ovsdpdk /build
+sudo docker build -t openretriever/cnicniovsdpdk . -f Dockerfile.cniovsdpdk
+sudo rm -rf $HOME/cni/ovsdpdk
+echo git clone https://github.com/containernetworking/cni
+echo sudo CNI_PATH=$CNI_PATH ./priv-net-run.sh ifconfig
+echo sudo docker login openretriever/cnicniovsdpdk -u user -p password
+echo sudo docker push openretriever/cnicniovsdpdk
+cd $PWD
