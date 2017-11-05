9bf4a114bea41b9ca7fb172ce9bf83283c72d36f
Guo Ruijing <ruijing.guo@intel.com>
2017-11-05 10:01:07 -0800

    Add more deploy in container4nfv daily jobs
    Change-Id: Ie42f991d6c3690fe4970bcc737777a36d76eeb90
    Signed-off-by: Guo Ruijing <ruijing.guo@intel.com>

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -18,7 +18,6 @@
 set -ex
 
 ../src/vagrant/kubeadm_basic/deploy.sh
-#enable later
-#../src/vagrant/kubeadm_ovsdpdk/deploy.sh
-#../src/vagrant/kubeadm_multus/deploy.sh
-#../src/vagrant/kubeadm_virtlet/deploy.sh
+../src/vagrant/kubeadm_ovsdpdk/deploy.sh
+../src/vagrant/kubeadm_multus/deploy.sh
+../src/vagrant/kubeadm_virtlet/deploy.sh
