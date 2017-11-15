d2116720abfb794375875c1b41c0bc06e2f3e7db
Guo Ruijing <ruijing.guo@intel.com>
2017-11-15 16:57:00 -0500

    change to docker 1.12.6 from dockerproject.org
    Change-Id: Ic33efb5e204d63cfd870f47c4fd8fb8e1dbf7af1
    Signed-off-by: Guo Ruijing <ruijing.guo@intel.com>

--- src/cni/ovsdpdk/build.sh
+++ src/cni/ovsdpdk/build.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
 set -ex
 
-HOME="$(dirname `readlink -f $0`)"
+DIR="$(dirname `readlink -f $0`)"
 PWD="$(pwd)"
 
-cd $HOME
-sudo apt-get install -y docker.io
+cd $DIR
+
+sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+sudo apt-key adv -k 58118E89F3A912897C070ADBF76221572C52609D
+cat << EOF | sudo tee /etc/apt/sources.list.d/docker.list
+deb [arch=amd64] https://apt.dockerproject.org/repo ubuntu-xenial main
+EOF
+
+sudo apt-get install -y --allow-downgrades docker-engine=1.12.6-0~ubuntu-xenial
 sudo docker rmi -f cniovsdpdk.build || true
 sudo docker build -t cniovsdpdk.build . -f Dockerfile.build
-sudo docker run -v $HOME/cni:/build -t cniovsdpdk.build cp /plugins/bin/ovsdpdk /build
+sudo docker run -v $DIR/cni:/build -t cniovsdpdk.build cp /plugins/bin/ovsdpdk /build
 sudo docker build -t openretriever/cnicniovsdpdk . -f Dockerfile.cniovsdpdk
-sudo rm -rf $HOME/cni/ovsdpdk
+sudo rm -rf $DIR/cni/ovsdpdk
 echo git clone https://github.com/containernetworking/cni
 echo sudo CNI_PATH=$CNI_PATH ./priv-net-run.sh ifconfig
 echo sudo docker login openretriever/cnicniovsdpdk -u user -p password
--- src/vagrant/kubeadm_ovsdpdk/host_setup.sh
+++ src/vagrant/kubeadm_ovsdpdk/host_setup.sh
@@ -10,12 +10,18 @@ cat << EOF | sudo tee /etc/hosts
 192.168.1.23 worker3
 EOF
 
+sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+sudo apt-key adv -k 58118E89F3A912897C070ADBF76221572C52609D
+cat << EOF | sudo tee /etc/apt/sources.list.d/docker.list
+deb [arch=amd64] https://apt.dockerproject.org/repo ubuntu-xenial main
+EOF
+
 curl -s http://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 cat <<EOF | sudo tee /etc/apt/sources.list.d/kubernetes.list
 deb http://apt.kubernetes.io/ kubernetes-xenial main
 EOF
 sudo apt-get update
-sudo apt-get install -y --allow-downgrades docker.io=1.12.6-0ubuntu1~16.04.1 kubelet=1.7.0-00 kubeadm=1.7.0-00 kubectl=1.7.0-00 kubernetes-cni=0.5.1-00
+sudo apt-get install -y --allow-downgrades docker-engine=1.12.6-0~ubuntu-xenial kubelet=1.7.0-00 kubeadm=1.7.0-00 kubectl=1.7.0-00 kubernetes-cni=0.5.1-00
 
 sudo rm -rf /var/lib/kubelet
 sudo systemctl stop kubelet
