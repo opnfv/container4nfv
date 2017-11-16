b79eb412f668738064689a4342d4191a521c7a34
Guo Ruijing <ruijing.guo@intel.com>
2017-11-16 12:23:16 -0500

    add examples for container4nfv
    1. add example for kubeadm with weave
    2. add example for kubeadm with multus
    3. change back to bento/ubuntu-16.04
    
    Change-Id: I138e71e45f0aba8389dca0e1a826f4662572af29
    Signed-off-by: Guo Ruijing <ruijing.guo@intel.com>

--- src/vagrant/kubeadm_basic/examples/nginx-app.sh
+++ src/vagrant/kubeadm_basic/examples/nginx-app.sh
@@ -3,8 +3,18 @@
 kubectl create -f /vagrant/examples/nginx-app.yaml
 kubectl get nodes
 kubectl get services
-kubectl get pods
 kubectl get rc
-sleep 120
+kubectl get pods
+
+r="0"
+while [ $r -ne "2" ]
+do
+   r=$(kubectl get pods | grep Running | wc -l)
+   sleep 20
+done
+
 svcip=$(kubectl get services nginx  -o json | grep clusterIP | cut -f4 -d'"')
 wget http://$svcip
+kubectl delete rc --all
+kubectl delete services --all
+kubectl delete pod --all
--- src/vagrant/kubeadm_multus/deploy.sh
+++ src/vagrant/kubeadm_multus/deploy.sh
@@ -4,5 +4,7 @@ set -ex
 DIR="$(dirname `readlink -f $0`)"
 
 cd $DIR
-vagrant up
 vagrant destroy -f
+vagrant up
+vagrant ssh master -c "/vagrant/examples/multus.sh" || (echo vagrant destroy -f; exit 1)
+echo vagrant destroy -f
