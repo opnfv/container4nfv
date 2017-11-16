11f613b0cc9f00093b55d46e5ea2a82fec77f1ab
Guo Ruijing <ruijing.guo@intel.com>
2017-11-16 12:23:16 -0500

    add examples for container4nfv
    1. add example for kubeadm with weave
    2. add example for kubeadm with multus
    3. add example for kubeadm with ovsdpdk
    4. add example for kubeadm with virtlet
    
    Change-Id: I138e71e45f0aba8389dca0e1a826f4662572af29
    Signed-off-by: Guo Ruijing <ruijing.guo@intel.com>

--- src/vagrant/kubeadm_multus/examples/multus.sh
+++ src/vagrant/kubeadm_multus/examples/multus.sh
@@ -17,8 +17,23 @@
 
 set -ex
 
-./cleanup.sh
-../src/vagrant/kubeadm_basic/deploy.sh
-../src/vagrant/kubeadm_ovsdpdk/deploy.sh
-../src/vagrant/kubeadm_multus/deploy.sh
-../src/vagrant/kubeadm_virtlet/deploy.sh
+#workaroud to fix dns pod issue
+times=0
+
+while [ $times -lt "3" ]
+do
+    kubectl get pods -n kube-system | grep kube-dns | grep -v Run | sed "s/ .*//" | \
+        xargs -I {} kubectl delete pod -n kube-system {}
+    sleep 20
+    times+=1
+done
+
+kubectl apply -f /vagrant/examples/busybox.yaml
+r="0"
+while [ $r -ne "2" ]
+do
+   r=$(kubectl get pods | grep Running | wc -l)
+   sleep 20
+done
+kubectl get pods --all-namespaces
+kubectl get pods | grep Run | sed "s/ .*//" | xargs -I {} kubectl exec -i {} ip a
--- src/vagrant/kubeadm_ovsdpdk/deploy.sh
+++ src/vagrant/kubeadm_ovsdpdk/deploy.sh
@@ -4,5 +4,8 @@ set -ex
 DIR="$(dirname `readlink -f $0`)"
 
 cd $DIR
+vagrant destroy -f
+rm -rf container-ipam-state
 vagrant up
+vagrant ssh master -c "/vagrant/examples/virtio-user.sh" || (vagrant destroy -f; exit 1)
 vagrant destroy -f
