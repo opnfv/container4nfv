43e65ebd8e00350d7bb0e1952030b2735a5fdd28
Guo Ruijing <ruijing.guo@intel.com>
2017-11-16 12:23:16 -0500

    add examples for container4nfv
    1. add example for kubeadm with weave
    2. add example for kubeadm with multus
    3. add example for kubeadm with ovsdpdk
    4. add example for kubeadm with virtlet
    
    Change-Id: I138e71e45f0aba8389dca0e1a826f4662572af29
    Signed-off-by: Guo Ruijing <ruijing.guo@intel.com>

--- src/vagrant/kubeadm_virtlet/examples/virtlet.sh
+++ src/vagrant/kubeadm_virtlet/examples/virtlet.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
 
+set -ex
+
 kubectl label node worker1 extraRuntime=virtlet
 kubectl label node worker2 extraRuntime=virtlet
-kubectl label node worker3 extraRuntime=virtlet
 kubectl create configmap -n kube-system virtlet-config --from-literal=download_protocol=http --from-literal=image_regexp_translation=1 --from-literal=disable_kvm=y
-kubectl create configmap -n kube-system virtlet-image-translations --from-file images.yaml
-kubectl create -f virtlet-ds.yaml
-kubectl create -f cirros-vm.yaml
+kubectl create configmap -n kube-system virtlet-image-translations --from-file /vagrant/examples/images.yaml
+kubectl create -f /vagrant/examples/virtlet-ds.yaml
+
+kubectl delete pod --all
+kubectl create -f /vagrant/examples/cirros-vm.yaml
+r="0"
+while [ $r -ne "1" ]
+do
+   r=$(kubectl get pods | grep Running | wc -l)
+   sleep 20
+done
+sleep 20
+kubectl get pods -o json | grep podIP | cut -f4 -d'"' | xargs ping -c 4
