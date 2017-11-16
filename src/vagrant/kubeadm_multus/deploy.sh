0f8bfc3e858418add05294d36f05ed26885bf86d
Guo Ruijing <ruijing.guo@intel.com>
2017-11-16 12:23:16 -0500

    add examples for container4nfv
    1. add example for kubeadm with weave
    2. add example for kubeadm with multus
    3. add example for kubeadm with ovsdpdk
    4. add example for kubeadm with virtlet
    
    Change-Id: I138e71e45f0aba8389dca0e1a826f4662572af29
    Signed-off-by: Guo Ruijing <ruijing.guo@intel.com>

--- src/vagrant/kubeadm_multus/deploy.sh
+++ src/vagrant/kubeadm_multus/deploy.sh
@@ -4,5 +4,7 @@ set -ex
 DIR="$(dirname `readlink -f $0`)"
 
 cd $DIR
+vagrant destroy -f
 vagrant up
+vagrant ssh master -c "/vagrant/examples/multus.sh" || (vagrant destroy -f; exit 1)
 vagrant destroy -f
