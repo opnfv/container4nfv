633800321ede277b05481a2ffbfabdab4842c99c
Guo Ruijing <ruijing.guo@intel.com>
2017-11-16 12:23:16 -0500

    add examples for container4nfv
    1. add example for kubeadm with weave
    2. add example for kubeadm with multus
    3. add example for kubeadm with ovsdpdk
    4. add example for kubeadm with virtlet
    
    Change-Id: I138e71e45f0aba8389dca0e1a826f4662572af29
    Signed-off-by: Guo Ruijing <ruijing.guo@intel.com>

--- src/vagrant/kubeadm_multus/master_setup.sh
+++ src/vagrant/kubeadm_multus/master_setup.sh
@@ -2,12 +2,11 @@
 
 set -ex
 
-sudo timeout 600 kubeadm init --apiserver-advertise-address=192.168.1.10 --service-cidr=10.96.0.0/24 --pod-network-cidr=10.244.0.0/16 --token 8c5adc.1cec8dbf339093f0
+sudo timeout 600 kubeadm init --apiserver-advertise-address=192.168.1.10 --service-cidr=10.96.0.0/16 --pod-network-cidr=10.32.0.0/12 --token 8c5adc.1cec8dbf339093f0
 sudo cp /etc/kubernetes/admin.conf $HOME/
 sudo chown $(id -u):$(id -g) $HOME/admin.conf
 export KUBECONFIG=$HOME/admin.conf
 echo "export KUBECONFIG=$HOME/admin.conf" >> $HOME/.bash_profile
 
 kubectl apply -f http://git.io/weave-kube-1.6
-kubectl apply -f http://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
 kubectl apply -f /vagrant/multus/kube_cni_multus.yml
