847fa1d17398e49d1780776685837f207ad9cf7f
Guo Ruijing <ruijing.guo@intel.com>
2017-11-22 13:06:39 -0500

    build multus from ubuntu:16.04 during deployment
    Change-Id: I557be21c59e28401c5ec20cd43d2baabc13bef3e
    Signed-off-by: Guo Ruijing <ruijing.guo@intel.com>

--- src/vagrant/kubeadm_multus/multus/setup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-BUILD=`pwd`
-
-sudo docker build -t multus.build . -f Dockerfile.build
-sudo docker run -v $BUILD:/build -ti multus.build cp multus-cni/bin/multus /build 
-sudo docker build -t openretriever/cnimultus . -f Dockerfile.multus
-sudo docker push openretriever/cnimultus
-rm -rf multus
