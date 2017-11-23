6fc26ce5956962e89fea301d28272c1c974af501
Guo Ruijing <ruijing.guo@intel.com>
2017-11-23 12:58:05 -0500

    build ovsdpdk cni during deployment
    Change-Id: I1c3d4b44bab0d75fae93f7fabce7bcfdcc3b9f35
    Signed-off-by: Guo Ruijing <ruijing.guo@intel.com>

--- src/cni/ovsdpdk/build.sh
+++ src/cni/ovsdpdk/build.sh
@@ -2,24 +2,6 @@
 set -ex
 
 DIR="$(dirname `readlink -f $0`)"
-PWD="$(pwd)"
 
 cd $DIR
-
-sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
-sudo apt-key adv -k 58118E89F3A912897C070ADBF76221572C52609D
-cat << EOF | sudo tee /etc/apt/sources.list.d/docker.list
-deb [arch=amd64] https://apt.dockerproject.org/repo ubuntu-xenial main
-EOF
-
-sudo apt-get install -y --allow-downgrades docker-engine=1.12.6-0~ubuntu-xenial
-sudo docker rmi -f cniovsdpdk.build || true
-sudo docker build -t cniovsdpdk.build . -f Dockerfile.build
-sudo docker run -v $DIR/cni:/build -t cniovsdpdk.build cp /plugins/bin/ovsdpdk /build
-sudo docker build -t openretriever/cnicniovsdpdk . -f Dockerfile.cniovsdpdk
-sudo rm -rf $DIR/cni/ovsdpdk
-echo git clone https://github.com/containernetworking/cni
-echo sudo CNI_PATH=$CNI_PATH ./priv-net-run.sh ifconfig
-echo sudo docker login openretriever/cnicniovsdpdk -u user -p password
-echo sudo docker push openretriever/cnicniovsdpdk
-cd $PWD
+sudo docker run -v $DIR:/build ubuntu:16.04 /build/install_cni.sh
---
+++ src/cni/ovsdpdk/install_cni.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -ex
+
+
+export PATH=/usr/local/go/bin:$PATH
+
+DIR="$(dirname `readlink -f $0`)"
+cd $DIR
+apt-get update && apt-get install -y git wget gcc
+wget -qO- https://storage.googleapis.com/golang/go1.8.3.linux-amd64.tar.gz | tar -C /usr/local/ -xz
+rm -rf plugins
+git clone https://github.com/containernetworking/plugins
+cd plugins
+git checkout 5544d9ced0d6e908fe26e9dbe529c7feb87d21f5
+mkdir plugins/main/ovsdpdk
+cp ../ovsdpdk.go plugins/main/ovsdpdk
+sed -i "s,PLUGINS=.*,PLUGINS=plugins/main/ovsdpdk," build.sh
+./build.sh
+mkdir -p /opt/cni/bin
+cp bin/ovsdpdk /opt/cni/bin
+cp ../setup_ovsdpdk.sh ../teardown_ovsdpdk.sh /opt/cni/bin
