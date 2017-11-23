a8388222d5d1f11be506e9e79f5e2476c447177e
Guo Ruijing <ruijing.guo@intel.com>
2017-11-23 12:58:05 -0500

    build ovsdpdk cni during deployment
    Change-Id: I1c3d4b44bab0d75fae93f7fabce7bcfdcc3b9f35
    Signed-off-by: Guo Ruijing <ruijing.guo@intel.com>

--- src/vagrant/kubeadm_ovsdpdk/ovsdpdk/install.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-set -ex
-sudo apt-get update
-sudo apt-get install -y openvswitch-switch-dpdk pciutils vim
-sudo update-alternatives --set ovs-vswitchd /usr/lib/openvswitch-switch-dpdk/ovs-vswitchd-dpdk
