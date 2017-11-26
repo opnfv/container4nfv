451d2d3c1bac43c641b45dab06babba624a1ecd7
Guo Ruijing <ruijing.guo@intel.com>
2017-11-26 14:37:02 -0500

    fix verify jenkins failure
    Change-Id: I0d9d33ea263112c627122d5af570578bc3f2284f
    Signed-off-by: Guo Ruijing <ruijing.guo@intel.com>

--- src/cni/ovsdpdk/install_cni.sh
+++ src/cni/ovsdpdk/install_cni.sh
@@ -13,10 +13,9 @@ git clone https://github.com/containernetworking/plugins
 cd plugins
 git checkout 5544d9ced0d6e908fe26e9dbe529c7feb87d21f5
 mkdir plugins/main/ovsdpdk
-cp ../ovsdpdk.go plugins/main/ovsdpdk
+cp $DIR/ovsdpdk.go plugins/main/ovsdpdk
 sed -i "s,PLUGINS=.*,PLUGINS=plugins/main/ovsdpdk," build.sh
 ./build.sh
 mkdir -p /opt/cni/bin
 cp bin/ovsdpdk /opt/cni/bin
-cp ../setup_ovsdpdk.sh ../teardown_ovsdpdk.sh /opt/cni/bin
-rm -rf plugins
+cp $DIR/setup_ovsdpdk.sh $DIR/teardown_ovsdpdk.sh /opt/cni/bin
