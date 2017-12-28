9feaebbf53e7efaf9cd9f56a16720eae0a0bef87
Guo Ruijing <ruijing.guo@intel.com>
2017-12-28 11:43:03 -0500

    reflactor virtio-user vnf
    Change-Id: I9e4d3f423b48920b645a826ce6e8907c14f23dfd
    Signed-off-by: Guo Ruijing <ruijing.guo@intel.com>

--- src/vnf/virtio-user-ping/setup_vpp.sh
+++ src/vnf/virtio-user-ping/setup_vpp.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
+
+DIR=$(dirname `readlink -f $0`)
+export PATH=$DIR/bin:$PATH
+export LD_LIBRARY_PATH=$DIR/bin/lib64
+
+rm -rf var/run/.rte_config
+id=$(ip a | grep dummy- | cut -f2 -d"-" | cut -f1 -d":")
+sed -i "s/vhost-user-1/vhost-user-$id/" $DIR/startup.conf
 mkdir -p /run/vpp
-vpp -c /root/startup.conf &
+vpp -c $DIR/startup.conf &
 sleep 10
 chmod 777 /run/vpp/cli.sock
 vppctl set int state VirtioUser0/0/0 up
-vppctl set int ip add VirtioUser0/0/0 192.168.3.2/24
+ip=$(cut -f6 -d":" /var/run/cni/netconf-$id | cut -f1 -d" ")
+vppctl set int ip add VirtioUser0/0/0 $ip/24
 sleep 1000000
