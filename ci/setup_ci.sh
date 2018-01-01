56a7275c8c8b1fefb9ed36230a2487cd89cf1bbe
Guo Ruijing <ruijing.guo@intel.com>
2018-01-01 11:38:23 -0500

    update CI setup in container4nfv
    Change-Id: Ic87532b6101a07f0c6c2a9ee0a8e2fbd4f2f1d70
    Signed-off-by: Guo Ruijing <ruijing.guo@intel.com>

--- ci/setup_ci.sh
+++ ci/setup_ci.sh
@@ -6,6 +6,6 @@ echo "Please run it by jenkins account!"
 cat << EOF | sudo tee /etc/sudoers.d/${USER}
 ${USER} ALL = (root) NOPASSWD:ALL
 EOF
-sudo apt-get update > /dev/null
-sudo apt install -y qemu-kvm libvirt-bin 2>1 /dev/null
-echo "Plsease reboot/logout to make effective for libvirt group adding"
+../src/vagrant/setup_vagrant.sh
+echo "###########################################################"
+echo "Please reboot to make effective for libvirt group adding!!!"
