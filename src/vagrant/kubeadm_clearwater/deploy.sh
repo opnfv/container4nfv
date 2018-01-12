e5723d276fa8861f863637917af689ce4eeae5a2
Laura Sofia Enriquez <lsofia.enriquez@gmail.com>
2018-01-12 02:10:16 -0300

    Clearwater implementation for OPNFV K8S version 1.9
    Clearwater deployment from source code using k8s v1.9.
    Tests added.
    
    Change-Id: Ifde573768721871b441ecb06a60ac25c0519b73d
    Signed-off-by: Laura Sofia Enriquez <lsofia.enriquez@gmail.com>

--- src/vagrant/kubeadm_clearwater/deploy.sh
+++ src/vagrant/kubeadm_clearwater/deploy.sh
@@ -6,4 +6,7 @@ DIR="$(dirname `readlink -f $0`)"
 cd $DIR
 ../cleanup.sh
 vagrant up
-vagrant ssh master -c "/vagrant/examples/create_and_apply.sh"
+vagrant ssh master -c "/vagrant/clearwater_setup.sh"
+
+# Run tests
+vagrant ssh master -c "/vagrant/tests/clearwater-live-test.sh"
