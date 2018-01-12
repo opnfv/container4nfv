45fe5a481a823acc187dd092d939e003b6a4a8a5
Laura Sofia Enriquez <lsofia.enriquez@gmail.com>
2018-01-12 02:10:16 -0300

    [WIP] VNF: Clearwater implementation for OPNFV K8S version 1.9
    This patch will have all the scripts to run e2e the
    Clearwater project.
    
    Change-Id: Ifde573768721871b441ecb06a60ac25c0519b73d
    Signed-off-by: Laura Sofia Enriquez <lsofia.enriquez@gmail.com>

--- src/vagrant/kubeadm_clearwater/deploy.sh
+++ src/vagrant/kubeadm_clearwater/deploy.sh
@@ -6,4 +6,4 @@ DIR="$(dirname `readlink -f $0`)"
 cd $DIR
 ../cleanup.sh
 vagrant up
-vagrant ssh master -c "/vagrant/examples/create_and_apply.sh"
+vagrant ssh master -c "/vagrant/clearwater/clearwater_setup.sh"
