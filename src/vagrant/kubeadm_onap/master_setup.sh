f223641a250f6305763056c9e65db30097592158
Xuan Jia <jason.jiaxuan@gmail.com>
2018-01-15 22:58:21 +0800

    Create helm-charts and nfv-pass directory
    Change-Id: I4fe3ebe772d5f6af60ca86d7175de7d8ecb94f6c
    Signed-off-by: Xuan Jia <jason.jiaxuan@gmail.com>

--- src/vagrant/kubeadm_onap/master_setup.sh
+++ src/vagrant/kubeadm_onap/master_setup.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 set -ex
-
+export http_proxy=192.168.39.9:8118
+export https_proxy=192.168.39.9:8118
 sudo kubeadm init --apiserver-advertise-address=192.168.0.10  --service-cidr=10.96.0.0/24 --pod-network-cidr=10.32.0.0/12 --token 8c5adc.1cec8dbf339093f0
 mkdir ~/.kube
 sudo cp /etc/kubernetes/admin.conf ~/.kube/config
--- src/vagrant/kubeadm_onap/worker_setup.sh
+++ src/vagrant/kubeadm_onap/worker_setup.sh
@@ -4,6 +4,8 @@ set -ex
 
 sudo mkdir /dockerdata-nfs
 sudo chmod 755 /dockerdata-nfs
+export http_proxy=192.168.39.9:8118
+export https_proxy=192.168.39.9:8118
 sudo kubeadm join --token 8c5adc.1cec8dbf339093f0 192.168.0.10:6443 || true
 
 sudo apt-get install -y putty-tools
