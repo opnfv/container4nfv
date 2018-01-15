528e4e6294d2e367b97ba2d0e44e2b3f00ba9b85
Xuan Jia <jason.jiaxuan@gmail.com>
2018-01-15 22:58:21 +0800

    Create helm-charts and nfv-pass directory
    Change-Id: I4fe3ebe772d5f6af60ca86d7175de7d8ecb94f6c
    Signed-off-by: Xuan Jia <jason.jiaxuan@gmail.com>

--- src/vagrant/kubeadm_onap/host_setup.sh
+++ src/vagrant/kubeadm_onap/host_setup.sh
@@ -16,11 +16,21 @@ sudo apt-key adv -k 58118E89F3A912897C070ADBF76221572C52609D
 cat << EOF | sudo tee /etc/apt/sources.list.d/docker.list
 deb [arch=amd64] https://apt.dockerproject.org/repo ubuntu-xenial main
 EOF
-
+export http_proxy=192.168.39.9:8118
+export https_proxy=192.168.39.9:8118
 curl -s http://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
-cat <<EOF | sudo tee /etc/apt/sources.list.d/kubernetes.list
+unset http_proxy
+unset https_proxy
+
+cat << EOF | sudo tee /etc/apt/sources.list.d/kubernetes.list
 deb http://apt.kubernetes.io/ kubernetes-xenial main
 EOF
+
+sudo touch /etc/apt/apt.conf
+cat << EOF | sudo tee /etc/apt/apt.conf
+Acquire::http::Proxy "http://192.168.39.9:8118";
+EOF
+
 sudo apt-get update
 sudo apt-get install -y --allow-downgrades docker-engine=1.12.6-0~ubuntu-xenial kubelet=1.7.0-00 kubeadm=1.7.0-00 kubectl=1.7.0-00 kubernetes-cni=0.5.1-00
 
@@ -30,6 +40,14 @@ cat << EOF | sudo tee /etc/docker/daemon.json
   "storage-driver": "overlay"
 }
 EOF
+
+sudo mkdir /etc/systemd/system/docker.service.d
+cat << EOF | sudo tee /etc/systemd/system/docker.service.d/http-proxy.conf
+> [Service]
+> Environment="HTTP_PROXY=http://192.168.39.9:8118/"
+> Environment="HTTPS_PROXY=http://192.168.39.9:8118/"
+EOF
+
 sudo systemctl daemon-reload
 sudo systemctl start docker
 
