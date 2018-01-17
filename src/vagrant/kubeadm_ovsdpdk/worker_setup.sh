9625ffb3ef0f75db1ae84fe34269cd26d10eef1b
Laura Sofia Enriquez <lsofia.enriquez@gmail.com>
2018-01-16 21:43:48 -0300

    Upgrade kubeadm_ovsdpdk to k8s 1.9
    Upgrade kubeadm_ovsdpdk project to use k8s 1.9.
    
    Change-Id: I1a3a8d21ee906acfa13003d66dde3eb3ee7595ea
    Signed-off-by: Laura Sofia Enriquez <lsofia.enriquez@gmail.com>

--- src/vagrant/kubeadm_ovsdpdk/worker_setup.sh
+++ src/vagrant/kubeadm_ovsdpdk/worker_setup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 set -ex
-sudo kubeadm join --token 8c5adc.1cec8dbf339093f0 192.168.1.10:6443 || true
+sudo kubeadm join --discovery-token-unsafe-skip-ca-verification --token 8c5adc.1cec8dbf339093f0 192.168.1.10:6443 || true
