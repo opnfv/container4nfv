11f967b8e1caec6c2b0e8bbe35b2525e30ae9615
Guo Ruijing <ruijing.guo@intel.com>
2018-02-12 03:48:10 -0500

    update kata container deployment
        1. fix kata container deployment failure issue
        2. use package instead of source build
        3. remove docker installation
        4. upgrade to k8s 1.9
    
    Change-Id: Ie7698fb0e6a3a459d42b17d998b21f2dcf3a7854
    Signed-off-by: Guo Ruijing <ruijing.guo@intel.com>

--- src/vagrant/kubeadm_kata/master_setup.sh
+++ src/vagrant/kubeadm_kata/master_setup.sh
@@ -17,7 +17,7 @@
 
 set -ex
 
-sudo kubeadm init --apiserver-advertise-address=192.168.1.10  --service-cidr=10.96.0.0/16 --pod-network-cidr=10.32.0.0/12 --token 8c5adc.1cec8dbf339093f0
+sudo kubeadm init --skip-preflight-checks --apiserver-advertise-address=192.168.1.10  --service-cidr=10.96.0.0/16 --pod-network-cidr=10.32.0.0/12 --token 8c5adc.1cec8dbf339093f0
 mkdir ~/.kube
 sudo cp /etc/kubernetes/admin.conf .kube/config
 sudo chown $(id -u):$(id -g) ~/.kube/config
