40a43fd636dec7453d4f3f59ac5c4f626023bdf4
QiLiang <liangqi1@huawei.com>
2018-03-07 14:45:30 +0800

    Basic setup for istio sidecar auto injection
    To enable auto sidecar injection on specific namespace,
    you only need label the namespace with `istio-injection=enabled`, e.g.:
        kubectl label namespace default istio-injection=enabled
    
    For details please refer:
        https://istio.io/docs/setup/kubernetes/sidecar-injection.html#automatic-sidecar-injection
    
    Change-Id: I2059aa0be2ab3f4f942342850d286281c5f940d4
    Signed-off-by: QiLiang <liangqi1@huawei.com>

--- src/vagrant/kubeadm_istio/master_setup.sh
+++ src/vagrant/kubeadm_istio/master_setup.sh
@@ -8,3 +8,24 @@ sudo cp /etc/kubernetes/admin.conf $HOME/.kube/config
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
 
 kubectl apply -f http://git.io/weave-kube-1.6
+
+# Enable mutating webhook admission controller
+# https://istio.io/docs/setup/kubernetes/sidecar-injection.html
+export ADMISSION_CONTROL="Initializers,NamespaceLifecycle,LimitRanger,ServiceAccount,DefaultStorageClass,DefaultTolerationSeconds,MutatingAdmissionWebhook,ValidatingAdmissionWebhook,NodeRestriction,ResourceQuota"
+export KUBE_APISERVER_CONF="/etc/kubernetes/manifests/kube-apiserver.yaml"
+sed -i "s/admission-control=.*/admission-control=$ADMISSION_CONTROL/g" $KUBE_APISERVER_CONF
+
+set +e
+# wait for kube-apiserver restart
+r="1"
+while [ $r -ne "0" ]
+do
+   sleep 2
+   kubectl version > /dev/null
+   r=$?
+done
+set -e
+
+# check if admissionregistration.k8s.io/v1beta1 API is enabled
+kubectl api-versions | grep admissionregistration
+
