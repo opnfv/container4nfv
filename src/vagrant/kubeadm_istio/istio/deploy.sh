f5e9d072a9d4d896a9da40dbfef8a5ca9abd7924
wutianwei <wutianwei1@huawei.com>
2018-06-06 06:50:33 -0400

    fix istio deploy fialure
    Some files don't exist in istio 0.8.0 version.
    So it failed to deploy istio in CI.
    what this patch do:
    1. using istio-demo.yml instead of istio.yml to deploy istio
    2. define the ingress gateway when running bookinfo sample
    3. rename ingress to ingressgateway
    
    Change-Id: I39ec39320d61e5af8282c6f50bad90240444bc79
    Signed-off-by: wutianwei <wutianwei1@huawei.com>

--- src/vagrant/kubeadm_istio/istio/deploy.sh
+++ src/vagrant/kubeadm_istio/istio/deploy.sh
@@ -35,21 +35,7 @@ echo 'export PATH="$PATH:/vagrant/istio-source/bin"' >> ~/.bashrc
 echo "source <(kubectl completion bash)" >> ~/.bashrc
 source ~/.bashrc
 
-kubectl apply -f install/kubernetes/istio.yaml
-
-# Install the sidecar injection configmap
-./install/kubernetes/webhook-create-signed-cert.sh \
-    --service istio-sidecar-injector \
-    --namespace istio-system \
-    --secret sidecar-injector-certs
-kubectl apply -f install/kubernetes/istio-sidecar-injector-configmap-release.yaml
-
-# Install the sidecar injector webhook
-cat install/kubernetes/istio-sidecar-injector.yaml | \
-     ./install/kubernetes/webhook-patch-ca-bundle.sh > \
-     install/kubernetes/istio-sidecar-injector-with-ca-bundle.yaml
-kubectl apply -f install/kubernetes/istio-sidecar-injector-with-ca-bundle.yaml
-kubectl -n istio-system get deployment -listio=sidecar-injector
+kubectl apply -f install/kubernetes/istio-demo.yaml
 
 # Validate the installation
 kubectl get svc -n istio-system
