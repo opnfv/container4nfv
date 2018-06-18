dcacd1a70a950b94b5f8886ce5672f202fe72b49
Guo Ruijing <ruijing.guo@intel.com>
2018-06-18 17:02:13 -0400

    enable k8s + ovn
    Change-Id: Ie5a1fecbf43ac479c7f78999ad212faf1ee3d932
    Signed-off-by: Guo Ruijing <ruijing.guo@intel.com>

---
+++ src/vagrant/kubeadm_ovn/master_setup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -ex
+
+sudo kubeadm init --apiserver-advertise-address=192.168.1.10  --service-cidr=10.96.0.0/16 --pod-network-cidr=10.32.0.0/12 --token 8c5adc.1cec8dbf339093f0
+mkdir ~/.kube
+sudo cp /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
+
+kubectl create serviceaccount ovn -n kube-system
+kubectl create clusterrolebinding --user system:serviceaccount:kube-system:ovn kube-system-cluster-admin --clusterrole cluster-admin
+
+sudo ovnkube -k8s-kubeconfig /home/vagrant/.kube/config -net-controller -loglevel=4 -k8s-apiserver=http://192.168.1.10:8080 -logfile=/var/log/openvswitch/ovnkube.log -init-master=master -cluster-subnet=10.32.0.0/12 -service-cluster-ip-range=10.96.0.0/16 -nodeport -nb-address=tcp://192.168.1.10:6631 -sb-address=tcp://192.168.1.10:6632 &
+
+kubectl proxy --address=192.168.1.10 --port=8080 &
