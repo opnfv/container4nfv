e9b8cd786d7908477412f394bdf4557a651758b0
Guo Ruijing <ruijing.guo@intel.com>
2018-07-30 12:21:36 -0400

    several enhancement/fix for kubeadm onap
    1. add ntp for kubeadm onap
    2. add mtu for tunnel interface cross 2 hosts
    3. add beijing branch
    
    Change-Id: I50faf8448ddc0efa765b98fecd19c1d7a52bf3a3
    Signed-off-by: Guo Ruijing <ruijing.guo@intel.com>

--- src/vagrant/kubeadm_onap/worker_setup.sh
+++ src/vagrant/kubeadm_onap/worker_setup.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 set -ex
 
+sudo apt-get -y install ntp
+cat << EOF | sudo tee /etc/ntp.conf
+pool master
+EOF
+sudo service ntp restart
+
 sudo kubeadm join --discovery-token-unsafe-skip-ca-verification --token 8c5adc.1cec8dbf339093f0 192.168.0.10:6443 || true
 
 sudo apt-get install nfs-common -y
