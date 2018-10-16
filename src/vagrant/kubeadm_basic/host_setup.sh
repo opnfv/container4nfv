ba5b53a60a7b3357692a8254822fec4f1489d3f2
Guo Ruijing <ruijing.guo@intel.com>
2018-10-16 09:42:30 -0400

    rebase to k8s 1.11 & docker 1.13 and enable device plugin
    Change-Id: Ifbe25c03de1c11144d093fe710b2a48c5c419b89
    Signed-off-by: Guo Ruijing <ruijing.guo@intel.com>

--- src/vagrant/kubeadm_basic/host_setup.sh
+++ src/vagrant/kubeadm_basic/host_setup.sh
@@ -21,8 +21,13 @@ cat <<EOF | sudo tee /etc/apt/sources.list.d/kubernetes.list
 deb http://apt.kubernetes.io/ kubernetes-xenial main
 EOF
 sudo apt-get update
-sudo apt-get install -y --allow-unauthenticated --allow-downgrades docker-engine=1.12.6-0~ubuntu-xenial kubelet=1.9.1-00 kubeadm=1.9.1-00 kubectl=1.9.1-00 kubernetes-cni=0.6.0-00
+sudo apt-get install -y --allow-unauthenticated docker-engine=1.13.1-0~ubuntu-xenial kubelet=1.11.3-00 kubeadm=1.11.3-00 kubectl=1.11.3-00 kubernetes-cni=0.6.0-00
+sudo sed -i '9i\Environment="KUBELET_EXTRA_ARGS=--feature-gates=DevicePlugins=true"' /etc/systemd/system/kubelet.service.d/10-kubeadm.conf
 
+sudo modprobe ip_vs
+sudo modprobe ip_vs_rr
+sudo modprobe ip_vs_wrr
+sudo modprobe ip_vs_sh
 sudo swapoff -a
 sudo systemctl daemon-reload
 sudo systemctl stop kubelet
