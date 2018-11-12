8c2602aa5e99381dfbf9210c0b4e3a82976052f3
Guo Ruijing <ruijing.guo@intel.com>
2018-11-12 21:13:11 +0800

    rebase virtlet
    1. k8s 1.12.2, docker: 18.03
    2. criproxy: 0.12.0, virtlet: v1.4.1
    
    Change-Id: I826bad4407ef4d048f3307dface5683f7d9d10e6
    Signed-off-by: Guo Ruijing <ruijing.guo@intel.com>

--- src/vagrant/kubeadm_virtlet/worker_setup.sh
+++ src/vagrant/kubeadm_virtlet/worker_setup.sh
@@ -1,18 +1,12 @@
 #!/bin/bash
 
 set -ex
-sudo kubeadm join --token 8c5adc.1cec8dbf339093f0 192.168.1.10:6443 || true
+sudo kubeadm join --discovery-token-unsafe-skip-ca-verification --token 8c5adc.1cec8dbf339093f0 192.168.1.10:6443
 
-sudo docker pull openretriever/virtlet
-sudo docker run --rm openretriever/virtlet tar -c /criproxy | sudo tar -C /usr/local/bin -xv
-sudo ln -s /usr/local/bin/criproxy /usr/local/bin/dockershim
-
-sudo mkdir /etc/criproxy
-sudo touch /etc/criproxy/node.conf
-sudo cp -r /vagrant/virtlet/etc/systemd/system/* /etc/systemd/system/
-sudo systemctl stop kubelet
+wget https://github.com/Mirantis/criproxy/releases/download/v0.12.0/criproxy_0.12.0_amd64.deb
+sudo dpkg -i criproxy_0.12.0_amd64.deb
+sudo sed -i "s/EnvironmentFile/#EnvironmentFile/" /etc/systemd/system/kubelet.service.d/10-kubeadm.conf
 sudo systemctl daemon-reload
-sudo systemctl enable criproxy dockershim
-sudo systemctl start criproxy dockershim
-sudo systemctl daemon-reload
-sudo systemctl start kubelet
+sudo systemctl restart dockershim
+sudo systemctl restart criproxy
+sudo systemctl restart kubelet
