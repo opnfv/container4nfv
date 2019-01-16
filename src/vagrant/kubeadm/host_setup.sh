7bc488fbca7fa6814bb426bebdc999be17beec2f
Guo Ruijing <ruijing.guo@intel.com>
2019-01-17 06:30:07 +0800

    some updates kata setup
    1. fix in containerd private registry
    2. master node using docker and worker node without docker
    3. rebase containerd to 1.2.2
    
    Change-Id: Ide2a45ed892134269995b8d96aa76d5c3362c6f3
    Signed-off-by: Guo Ruijing <ruijing.guo@intel.com>

--- src/vagrant/kubeadm/host_setup.sh
+++ src/vagrant/kubeadm/host_setup.sh
@@ -11,27 +11,6 @@ cat << EOF | sudo tee /etc/hosts
 192.168.1.23 worker3
 EOF
 
-sudo apt-get update
-sudo apt-get install -y \
-    apt-transport-https \
-    ca-certificates \
-    curl \
-    software-properties-common
-
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-   $(lsb_release -cs) \
-   stable"
-sudo apt-get update
-sudo apt-get install -y docker-ce=18.03.1~ce-0~ubuntu
-cat << EOF | sudo tee /etc/docker/daemon.json
-{
-    "insecure-registries": ["registry:5000"]
-}
-EOF
-sudo service docker restart
-
 curl -s http://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 cat <<EOF | sudo tee /etc/apt/sources.list.d/kubernetes.list
 deb http://apt.kubernetes.io/ kubernetes-xenial main
@@ -39,11 +18,14 @@ EOF
 sudo apt-get update
 sudo apt-get install -y --allow-unauthenticated kubelet=1.12.2-00 kubeadm=1.12.2-00 kubectl=1.12.2-00 kubernetes-cni=0.6.0-00
 echo 'Environment="KUBELET_EXTRA_ARGS=--feature-gates=DevicePlugins=true"' | sudo tee /etc/default/kubelet
-
+echo 1 | sudo tee /proc/sys/net/ipv4/ip_forward
 sudo modprobe ip_vs
 sudo modprobe ip_vs_rr
 sudo modprobe ip_vs_wrr
 sudo modprobe ip_vs_sh
+sudo modprobe br_netfilter
+sudo modprobe nf_conntrack_ipv4
+
 sudo swapoff -a
 sudo systemctl daemon-reload
 sudo systemctl stop kubelet
--- src/vagrant/kubeadm/master_setup.sh
+++ src/vagrant/kubeadm/master_setup.sh
@@ -2,6 +2,27 @@
 
 set -ex
 
+sudo apt-get update
+sudo apt-get install -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+sudo apt-get update
+sudo apt-get install -y docker-ce=18.03.1~ce-0~ubuntu
+cat << EOF | sudo tee /etc/docker/daemon.json
+{
+    "insecure-registries": ["registry:5000"]
+}
+EOF
+sudo service docker restart
+
 sudo kubeadm init --apiserver-advertise-address=192.168.1.10  --service-cidr=10.96.0.0/16 --pod-network-cidr=10.32.0.0/12 --token 8c5adc.1cec8dbf339093f0
 mkdir ~/.kube
 sudo cp /etc/kubernetes/admin.conf $HOME/.kube/config
