8d3c1306b354d63ec04f6372580afbeca147fe9a
Guo Ruijing <ruijing.guo@intel.com>
2019-02-16 00:16:49 -0500

    enable virtlet & kata
    Change-Id: I88f7b579a825ba7ad25da5074773e5668f0ef000
    Signed-off-by: Guo Ruijing <ruijing.guo@intel.com>

---
+++ src/vagrant/kubeadm/virtlet/virtlet.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -ex
+
+kubectl label node worker1 extraRuntime=virtlet
+kubectl label node worker2 extraRuntime=virtlet
+kubectl create configmap -n kube-system virtlet-config --from-literal=download_protocol=http --from-literal=image_regexp_translation=1 --from-literal=disable_kvm=y
+kubectl create configmap -n kube-system virtlet-image-translations --from-file /vagrant/virtlet/images.yaml
+kubectl create -f /vagrant/virtlet/virtlet-ds.yaml
+
+kubectl delete pod --all
+kubectl create -f /vagrant/virtlet/cirros-vm.yaml
+r="0"
+while [ $r -ne "1" ]
+do
+   r=$(kubectl get pods | grep Running | wc -l)
+   sleep 60
+done
+sleep 60
+kubectl get pods -o json | grep podIP | cut -f4 -d'"' | xargs ping -c 4
+echo 'login by user:cirros & password:gocubsgo'
