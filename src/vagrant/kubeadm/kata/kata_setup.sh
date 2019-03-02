ad783049bcefcf70f90f0ccded5b425f4c5dd4cc
Guo Ruijing <ruijing.guo@intel.com>
2019-03-01 23:00:29 -0500

    fix registry and kata installation
    Change-Id: Ie74c889ce0c8714f7e52beec9ace130350728472
    Signed-off-by: Guo Ruijing <ruijing.guo@intel.com>

--- src/vagrant/kubeadm/kata/kata_setup.sh
+++ src/vagrant/kubeadm/kata/kata_setup.sh
@@ -17,8 +17,9 @@
 
 set -ex
 
-sudo sh -c "echo 'deb http://download.opensuse.org/repositories/home:/katacontainers:/releases:/x86_64:/master/xUbuntu_16.04/ /' > /etc/apt/sources.list.d/kata-containers.list"
-curl -sL  http://download.opensuse.org/repositories/home:/katacontainers:/release/xUbuntu_$(lsb_release -rs)/Release.key | sudo apt-key add -
+ARCH=$(arch)
+sudo sh -c "echo 'deb http://download.opensuse.org/repositories/home:/katacontainers:/releases:/${ARCH}:/master/xUbuntu_$(lsb_release -rs)/ /' > /etc/apt/sources.list.d/kata-containers.list"
+curl -sL  http://download.opensuse.org/repositories/home:/katacontainers:/releases:/${ARCH}:/master/xUbuntu_$(lsb_release -rs)/Release.key | sudo apt-key add -
 sudo -E apt-get update
 sudo -E apt-get -y install kata-runtime kata-proxy kata-shim
 sudo -E apt-get -y install libseccomp2
--- src/vagrant/kubeadm/registry_setup.sh
+++ src/vagrant/kubeadm/registry_setup.sh
@@ -8,19 +8,7 @@ cat << EOF | sudo tee /etc/hosts
 EOF
 
 sudo apt-get update
-sudo apt-get install -y \
-    apt-transport-https \
-    ca-certificates \
-    curl \
-    software-properties-common
-
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-   $(lsb_release -cs) \
-   stable"
-sudo apt-get update
-sudo apt-get install -y docker-ce=18.06.0~ce~3-0~ubuntu
+sudo apt-get install -y docker.io
 cat << EOF | sudo tee /etc/docker/daemon.json
 {
     "insecure-registries": ["registry:5000"]
