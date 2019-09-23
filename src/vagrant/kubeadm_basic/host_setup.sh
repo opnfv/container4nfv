fdc43b308bddf2ca10ca4780e27ae0b91fc65fa6
Guo Ruijing <ruijing.guo@intel.com>
2019-09-23 17:02:21 +0800

    upgrade to ubuntu 18 & k8s 1.15
    Signed-off-by: Guo Ruijing <ruijing.guo@intel.com>
    Change-Id: I9c0905b5d2ef13219e0cf6369bb1321011270540

--- src/vagrant/kubeadm_basic/host_setup.sh
+++ src/vagrant/kubeadm_basic/host_setup.sh
@@ -2,6 +2,11 @@
 
 set -ex
 
+sudo systemctl stop systemd-resolved
+cat << EOF | sudo tee /etc/resolv.conf
+nameserver 8.8.8.8
+EOF
+
 cat << EOF | sudo tee /etc/hosts
 127.0.0.1    localhost
 192.168.1.10 master
@@ -11,26 +16,14 @@ cat << EOF | sudo tee /etc/hosts
 EOF
 
 sudo apt-get update
-sudo apt-get install -y \
-    apt-transport-https \
-    ca-certificates \
-    curl \
-    software-properties-common
-
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-   $(lsb_release -cs) \
-   stable"
-sudo apt-get update
-sudo apt-get install -y docker-ce=18.06.0~ce~3-0~ubuntu
+sudo apt-get install -y docker.io
 
 curl -s http://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 cat <<EOF | sudo tee /etc/apt/sources.list.d/kubernetes.list
 deb http://apt.kubernetes.io/ kubernetes-xenial main
 EOF
 sudo apt-get update
-sudo apt-get install -y --allow-unauthenticated kubelet=1.12.1-00 kubeadm=1.12.1-00 kubectl=1.12.1-00 kubernetes-cni=0.6.0-00
+sudo apt-get install -y --allow-unauthenticated kubelet=1.15.2-00 kubeadm=1.15.2-00 kubectl=1.15.2-00 kubernetes-cni=0.7.5-00
 sudo sed -i '9i\Environment="KUBELET_EXTRA_ARGS=--feature-gates=DevicePlugins=true"' /etc/systemd/system/kubelet.service.d/10-kubeadm.conf
 
 sudo modprobe ip_vs
